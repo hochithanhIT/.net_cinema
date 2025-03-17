84ec41ba0e4903b014dacd8bfde633d3f8c71dc0
NekoDao <149315150+NekoDaoL@users.noreply.github.com>
2025-03-17 19:03:22 +0700

    Temp commit before applying stash

--- Cinema/Form1.cs
+++ Cinema/Form1.cs
@@ -10,11 +10,83 @@ using System.Windows.Forms;
 
 namespace Cinema
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
+        private DataAccess dataAccess = new DataAccess();
+
         public Form1()
         {
             InitializeComponent();
+            UpdateLinkLabel();
+            LoadNowShowingMovies();
         }
+
+        private void UpdateLinkLabel()
+        {
+            if (Session.IsLoggedIn)
+            {
+                linkLabel1.Text = $"Hello, {Session.UserName}";
+            }
+            else
+            {
+                linkLabel1.Text = "Login";
+            }
+        }
+
+        private void LoadNowShowingMovies()
+        {
+            string sql = "SELECT * FROM Movie WHERE release_day <= GETDATE()";
+            DataTable dt = dataAccess.ExecuteQueryTable(sql);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void LoadComingSoonMovies()
+        {
+            string sql = "SELECT * FROM Movie WHERE release_day > GETDATE()";
+            DataTable dt = dataAccess.ExecuteQueryTable(sql);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (!Session.IsLoggedIn)
+            {
+                Form2 form2 = new Form2();
+                this.Hide();
+                if (form2.ShowDialog() == DialogResult.OK)
+                {
+                    UpdateLinkLabel();
+                    this.Show();
+                }
+            }
+            else
+            {
+                Form3 form3 = new Form3();
+                this.Hide();
+                form3.ShowDialog();
+                this.Show();
+            }
+        }
+
+        private void linkLabelNowShowing_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LoadNowShowingMovies();
+        }
+
+        private void linkLabelComingSoon_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LoadComingSoonMovies();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadNowShowingMovies();
+        }
+    }
+
+    public static class Session
+    {
+        public static bool IsLoggedIn { get; set; } = false;
+        public static string UserName { get; set; } = string.Empty;
     }
 }
---
+++ Cinema/Form2.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Cinema
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 228);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(853, 91);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "login/register form here";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "<<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 576);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
