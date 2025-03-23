eab7def2b254f99f13c2fa2b0591182202a5bf94
CTUKernel <nhanlahh@gmail.com>
2025-03-23 18:20:12 +0700

    Manage Seats by Date - Mov - Schedule - Theater

--- Cinema/Admin/UserControls/UC_Theaters.Designer.cs
+++ Cinema/Admin/UserControls/UC_Theaters.Designer.cs
@@ -34,6 +34,17 @@
             this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.FDate = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.FMovie = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.FSchedule = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,16 +62,17 @@
             // 
             this.TheaterCombobox.BackColor = System.Drawing.Color.Transparent;
             this.TheaterCombobox.BorderRadius = 15;
+            this.TheaterCombobox.BorderThickness = 2;
             this.TheaterCombobox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.TheaterCombobox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.TheaterCombobox.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
             this.TheaterCombobox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
-            this.TheaterCombobox.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold);
-            this.TheaterCombobox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.TheaterCombobox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.TheaterCombobox.ForeColor = System.Drawing.Color.Black;
             this.TheaterCombobox.ItemHeight = 30;
-            this.TheaterCombobox.Location = new System.Drawing.Point(48, 195);
+            this.TheaterCombobox.Location = new System.Drawing.Point(42, 586);
             this.TheaterCombobox.Name = "TheaterCombobox";
-            this.TheaterCombobox.Size = new System.Drawing.Size(274, 36);
+            this.TheaterCombobox.Size = new System.Drawing.Size(295, 36);
             this.TheaterCombobox.TabIndex = 5;
             // 
             // SeatPanel
@@ -96,19 +108,187 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.DarkGray;
-            this.label3.Location = new System.Drawing.Point(43, 141);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(37, 532);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(184, 26);
             this.label3.TabIndex = 9;
             this.label3.Text = "Choose a Theater";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // FDate
+            // 
+            this.FDate.BackColor = System.Drawing.Color.Transparent;
+            this.FDate.BorderRadius = 15;
+            this.FDate.Checked = true;
+            this.FDate.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.FDate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.FDate.ForeColor = System.Drawing.Color.White;
+            this.FDate.Format = System.Windows.Forms.DateTimePickerFormat.Long;
+            this.FDate.Location = new System.Drawing.Point(36, 170);
+            this.FDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
+            this.FDate.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
+            this.FDate.Name = "FDate";
+            this.FDate.Size = new System.Drawing.Size(301, 46);
+            this.FDate.TabIndex = 10;
+            this.FDate.Value = new System.DateTime(2025, 3, 23, 16, 19, 9, 416);
+            this.FDate.ValueChanged += new System.EventHandler(this.FDate_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(38, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 26);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Choose a movie";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // FMovie
+            // 
+            this.FMovie.BackColor = System.Drawing.Color.Transparent;
+            this.FMovie.BorderRadius = 15;
+            this.FMovie.BorderThickness = 2;
+            this.FMovie.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.FMovie.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FMovie.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.FMovie.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.FMovie.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.FMovie.ForeColor = System.Drawing.Color.Black;
+            this.FMovie.ItemHeight = 30;
+            this.FMovie.Location = new System.Drawing.Point(44, 305);
+            this.FMovie.Name = "FMovie";
+            this.FMovie.Size = new System.Drawing.Size(293, 36);
+            this.FMovie.TabIndex = 13;
+            this.FMovie.SelectedIndexChanged += new System.EventHandler(this.FMovie_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(38, 395);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(198, 26);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Choose a schedule";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // FSchedule
+            // 
+            this.FSchedule.BackColor = System.Drawing.Color.Transparent;
+            this.FSchedule.BorderRadius = 15;
+            this.FSchedule.BorderThickness = 2;
+            this.FSchedule.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.FSchedule.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FSchedule.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.FSchedule.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.FSchedule.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.FSchedule.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.FSchedule.ItemHeight = 30;
+            this.FSchedule.Location = new System.Drawing.Point(47, 440);
+            this.FSchedule.Name = "FSchedule";
+            this.FSchedule.Size = new System.Drawing.Size(288, 36);
+            this.FSchedule.TabIndex = 15;
+            this.FSchedule.SelectedIndexChanged += new System.EventHandler(this.FSchedule_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(749, 817);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 26);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Booked";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(1008, 817);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 26);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Available";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(37, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 26);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Choose a date";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // guna2Separator1
+            // 
+            this.guna2Separator1.BackColor = System.Drawing.Color.Silver;
+            this.guna2Separator1.Location = new System.Drawing.Point(383, 127);
+            this.guna2Separator1.Name = "guna2Separator1";
+            this.guna2Separator1.Size = new System.Drawing.Size(1, 750);
+            this.guna2Separator1.TabIndex = 20;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 15;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(673, 800);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(60, 60);
+            this.guna2Button1.TabIndex = 16;
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BorderRadius = 15;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.Gainsboro;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(927, 800);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.PressedColor = System.Drawing.Color.LightGray;
+            this.guna2Button2.PressedDepth = 0;
+            this.guna2Button2.Size = new System.Drawing.Size(60, 60);
+            this.guna2Button2.TabIndex = 18;
             // 
             // UC_Theaters
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.guna2Separator1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.FSchedule);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.FMovie);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.FDate);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.guna2PictureBox1);
@@ -117,6 +297,7 @@
             this.Controls.Add(this.label1);
             this.Name = "UC_Theaters";
             this.Size = new System.Drawing.Size(1360, 1024);
+            this.Load += new System.EventHandler(this.UC_Theaters_Load);
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -131,5 +312,16 @@
         private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2DateTimePicker FDate;
+        private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2ComboBox FMovie;
+        private System.Windows.Forms.Label label6;
+        private Guna.UI2.WinForms.Guna2ComboBox FSchedule;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
     }
 }
--- Cinema/Admin/UserControls/UC_Theaters.cs
+++ Cinema/Admin/UserControls/UC_Theaters.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,63 +19,91 @@ namespace Cinema.Admin.UserControls
         public UC_Theaters()
         {
             InitializeComponent();
-            LoadTheaters();
+            
             TheaterCombobox.SelectedIndexChanged += TheaterCombobox_SelectedIndexChanged;
         }
 
 
-        private void LoadTheaters()
-        {
-            string query = "SELECT id, theater_name FROM theater";
-            DataTable theaters = dataAccess.ExecuteQueryTable(query);
-
-            TheaterCombobox.DataSource = theaters;
-            TheaterCombobox.DisplayMember = "theater_name";
-            TheaterCombobox.ValueMember = "id";
-            
-
-
-            // Chọn rạp đầu tiên và load ghế
-            if (TheaterCombobox.Items.Count > 0)
-            {
-                TheaterCombobox.SelectedIndex = 0;
-                int firstTheaterId = Convert.ToInt32(TheaterCombobox.SelectedValue);
-                LoadSeats(firstTheaterId);
-            }
-        }
-
 
         private void TheaterCombobox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int selectedTheaterId = Convert.ToInt32(TheaterCombobox.SelectedValue);
-            LoadSeats(selectedTheaterId);
+            if (TheaterCombobox.SelectedValue != null)
+            {
+                int selectedTheaterId;
+
+                if (TheaterCombobox.SelectedValue is DataRowView rowView)
+                {
+                    // Lấy giá trị theater_id từ DataRowView
+                    selectedTheaterId = Convert.ToInt32(rowView["theater_id"]);
+                }
+                else if (int.TryParse(TheaterCombobox.SelectedValue.ToString(), out selectedTheaterId))
+                {
+                    // Chuyển đổi nếu SelectedValue là số nguyên hợp lệ
+                }
+                else
+                {
+                    MessageBox.Show("Lỗi: Không thể chuyển đổi giá trị của phòng chiếu.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                LoadSeats(selectedTheaterId);
+            }
         }
+
 
         private void LoadSeats(int theaterId)
         {
             SeatPanel.Controls.Clear(); // Xóa sơ đồ ghế cũ
 
-            string query = $"SELECT SEAT_CODE FROM seat WHERE THEATER_ID = {theaterId}";
-            DataTable seats = dataAccess.ExecuteQueryTable(query);
+            // Lấy danh sách tất cả ghế trong phòng chiếu
+            string querySeats = $"SELECT ID, SEAT_CODE FROM SEAT WHERE THEATER_ID = {theaterId}";
+            DataTable seats = dataAccess.ExecuteQueryTable(querySeats);
+
+            // Lấy danh sách các ghế đã được đặt từ bảng TICKET
+            string queryBookedSeats = $"SELECT SEAT_ID FROM TICKET WHERE SCHEDULE_ID IN (SELECT ID FROM SCHEDULE WHERE THEATER_ID = {theaterId})";
+            DataTable bookedSeats = dataAccess.ExecuteQueryTable(queryBookedSeats);
+
+            // Chuyển danh sách ghế đã đặt thành HashSet để tra cứu nhanh
+            HashSet<int> bookedSeatIds = new HashSet<int>();
+            foreach (DataRow row in bookedSeats.Rows)
+            {
+                bookedSeatIds.Add(Convert.ToInt32(row["SEAT_ID"]));
+            }
 
             int seatPerRow = 12; // Mỗi hàng có 12 ghế
             int index = 0;
 
             foreach (DataRow row in seats.Rows)
             {
+                int seatId = Convert.ToInt32(row["ID"]);
+                string seatCode = row["SEAT_CODE"].ToString();
+
                 Button seatButton = new Button();
-                seatButton.Text = row["SEAT_CODE"].ToString();
+                seatButton.Text = seatCode;
                 seatButton.Width = 60;
                 seatButton.Height = 60;
                 seatButton.Font = new Font("Arial", 8, FontStyle.Bold);
-                seatButton.BackColor = Color.LightGray;
                 seatButton.FlatStyle = FlatStyle.Flat;
                 seatButton.FlatAppearance.BorderSize = 0;
                 seatButton.Cursor = Cursors.Hand;
 
-                // Giảm độ bo tròn (Bo góc 15px thay vì bo tròn hoàn toàn)
+                // Kiểm tra nếu ghế đã được đặt
+                if (bookedSeatIds.Contains(seatId))
+                {
+                    seatButton.BackColor = Color.FromArgb(155, 63, 65); // Màu đỏ
+                    seatButton.ForeColor = Color.White; // Chữ màu trắng
+                    
+                }
+                else
+                {
+                    seatButton.BackColor = Color.Gainsboro; // Màu ghế trống
+                    seatButton.ForeColor = Color.Black; // Chữ màu đen
+                    seatButton.Enabled = true; // Có thể bấm
+                }
+
+                // Bo góc ghế
                 System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
-                int cornerRadius = 39; // Điều chỉnh mức độ bo tròn
+                int cornerRadius = 39;
                 path.AddArc(0, 0, cornerRadius, cornerRadius, 180, 90);
                 path.AddArc(seatButton.Width - cornerRadius, 0, cornerRadius, cornerRadius, 270, 90);
                 path.AddArc(seatButton.Width - cornerRadius, seatButton.Height - cornerRadius, cornerRadius, cornerRadius, 0, 90);
@@ -102,6 +131,208 @@ namespace Cinema.Admin.UserControls
             }
         }
 
+
+
+        private void FMovie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (FMovie.SelectedItem == null)
+            {
+                return;
+            }
+
+            try
+            {
+                // Kiểm tra nếu SelectedItem là DataRowView, thì lấy giá trị từ nó
+                if (FMovie.SelectedItem is DataRowView rowView)
+                {
+                    int selectedMovieID = Convert.ToInt32(rowView["id"]);
+                    LoadScheduleByMovie(selectedMovieID);
+                }
+                else
+                {
+                    int selectedMovieID = Convert.ToInt32(FMovie.SelectedValue);
+                    LoadScheduleByMovie(selectedMovieID);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi chọn phim: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+       
+
+
+
+        private void FDate_ValueChanged(object sender, EventArgs e)
+        {
+            LoadMoviesByDate();
+        }
+
+        private void LoadMoviesByDate()
+        {
+            DateTime selectedDate = FDate.Value;
+            string formattedDate = selectedDate.ToString("yyyy-MM-dd");
+
+            string query = $@"
+        SELECT DISTINCT m.id, m.movie_name
+        FROM schedule s
+        JOIN movie m ON s.movie_id = m.id
+        WHERE CAST(s.start_time AS DATE) = '{formattedDate}'";
+
+            DataTable movies = dataAccess.ExecuteQueryTable(query);
+
+            // Kiểm tra nếu không có phim nào
+            if (movies.Rows.Count == 0)
+            {
+                FMovie.DataSource = null; // Xóa dữ liệu cũ
+                FMovie.Items.Clear();
+                return;
+            }
+
+            FMovie.DataSource = movies;
+            FMovie.DisplayMember = "movie_name";
+            FMovie.ValueMember = "id";
+
+            // Chỉ đặt SelectedIndex nếu có dữ liệu
+            if (movies.Rows.Count > 0)
+            {
+                FMovie.SelectedIndex = 0;
+            }
+
+            // Điều chỉnh kích thước dropdown để hiển thị tên đầy đủ
+            FMovie.DropDownWidth = GetDropDownWidth(FMovie);
+        }
+
+
+        private int GetDropDownWidth(ComboBox myCombo)
+        {
+            int maxWidth = myCombo.Width;
+            using (Graphics g = myCombo.CreateGraphics())
+            {
+                foreach (DataRowView item in myCombo.Items)
+                {
+                    int itemWidth = (int)g.MeasureString(item["movie_name"].ToString(), myCombo.Font).Width + 10;
+                    maxWidth = Math.Max(maxWidth, itemWidth);
+                }
+            }
+            return maxWidth;
+        }
+
+
+        private void LoadScheduleByMovie(int movieID)
+        {
+            if (FDate.Value == null)
+            {
+                MessageBox.Show("Vui lòng chọn ngày hợp lệ!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DateTime selectedDate = FDate.Value;  // Lấy ngày đã chọn
+            string query = $@"
+        SELECT FORMAT(START_TIME, 'HH:mm') AS ShowTime 
+        FROM schedule 
+        WHERE MOVIE_ID = {movieID} 
+        AND CONVERT(date, START_TIME) = '{selectedDate:yyyy-MM-dd}' 
+        ORDER BY START_TIME";
+
+            try
+            {
+                DataAccess dataAccess = new DataAccess(); // Tạo kết nối từ lớp DataAccess
+                DataTable dt = dataAccess.ExecuteQueryTable(query);
+
+                // Xóa dữ liệu cũ và cập nhật danh sách suất chiếu
+                FSchedule.Items.Clear();
+                foreach (DataRow row in dt.Rows)
+                {
+                    FSchedule.Items.Add(row["ShowTime"].ToString());
+                }
+
+                // Kiểm tra nếu không có suất chiếu
+                if (FSchedule.Items.Count == 0)
+                {
+                    FSchedule.Items.Add("Không có suất chiếu nào");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi tải suất chiếu: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void FSchedule_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (FSchedule.SelectedItem == null || FMovie.SelectedValue == null)
+            {
+                return;
+            }
+
+            try
+            {
+                string selectedTime = FSchedule.SelectedItem.ToString();
+                int selectedMovieID = Convert.ToInt32(FMovie.SelectedValue);
+                DateTime selectedDate = FDate.Value;
+
+                string query = $@"
+        SELECT DISTINCT s.theater_id, t.theater_name
+        FROM schedule s
+        JOIN theater t ON s.theater_id = t.id
+        WHERE s.movie_id = {selectedMovieID}
+        AND FORMAT(s.start_time, 'HH:mm') = '{selectedTime}'
+        AND CONVERT(date, s.start_time) = '{selectedDate:yyyy-MM-dd}'
+        ORDER BY t.theater_name";
+
+                DataTable result = dataAccess.ExecuteQueryTable(query);
+
+                // Kiểm tra nếu có dữ liệu
+                if (result.Rows.Count > 0)
+                {
+                    TheaterCombobox.DataSource = result;
+                    TheaterCombobox.DisplayMember = "theater_name";
+                    TheaterCombobox.ValueMember = "theater_id";
+                    TheaterCombobox.SelectedIndex = 0; // Chọn phòng đầu tiên
+
+                    int selectedTheaterId = Convert.ToInt32(TheaterCombobox.SelectedValue);
+                    LoadSeats(selectedTheaterId); // Load sơ đồ ghế cho phòng đó
+                }
+                else
+                {
+                    TheaterCombobox.DataSource = null; // Không có phòng chiếu
+                    TheaterCombobox.Items.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi tải rạp: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UC_Theaters_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
 
