a680a322977a8721f4fc9119aee46fcbcbd1e7c0
Ho Chi Thanh <thanh271103@gmail.com>
2025-03-26 15:15:30 +0700

    ...

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -1,38 +1,28 @@
-﻿using Cinema.Forms;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Cinema.forms.profile;
 using Cinema.forms.SignUp_SignIn;
-using Cinema.homepage;
 
 namespace Cinema
 {
-    static class Program
-    {
+    static class Program {
         // Import API để bật DPI Awareness
         [DllImport("shcore.dll")]
         private static extern int SetProcessDpiAwareness(int awareness);
 
         [STAThread]
-        static void Main()
-        {
-            try
-            {
+        static void Main() {
+            try {
                 // Bật chế độ DPI cao (PerMonitorV2)
                 SetProcessDpiAwareness(2);
             }
-            catch (Exception)
-            {
+            catch (Exception) {
                 // Nếu không hỗ trợ, bỏ qua lỗi
             }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-          //TamNgo
+            //TamNgo
             Application.Run(new FormRegistration());
             //Application.Run(new TicketHistoryForm());
         }
---
+++ Cinema/homepage/FormRegistration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinema.forms.SignUp_SignIn
+{
+    public partial class FormRegistration : Form
+    {
+        public FormRegistration()
+        {
+            InitializeComponent();
+        }
+
+        public void AddUserControl(System.Windows.Forms.UserControl uc)
+        {
+            this.panelContainer.Controls.Clear();
+            this.panelContainer.Controls.Add(uc);
+            uc.Dock = DockStyle.Fill;
+            uc.BringToFront();
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            this.AddUserControl(new UCSignIn(this));
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            this.AddUserControl(new UCSignUp(this));
+        }
+
+        private void FormRegistration_Load_1(object sender, EventArgs e)
+        {
+            this.AddUserControl(new UCSignIn(this));
+        }
+
+        private void btnSignIn_MouseEnter(object sender, EventArgs e)
+        {
+            btnSignIn.BackColor = Color.White;
+            btnSignIn.ForeColor = Color.Black;
+        }
+
+        private void btnSignIn_MouseLeave(object sender, EventArgs e)
+        {
+            btnSignIn.BackColor = Color.Black;
+            btnSignIn.ForeColor = Color.White;
+        }
+
+        private void btnSignUp_MouseEnter(object sender, EventArgs e)
+        {
+            btnSignUp.BackColor = Color.WhiteSmoke;
+            btnSignUp.ForeColor = Color.Black;
+        }
+
+        private void btnSignUp_MouseLeave(object sender, EventArgs e)
+        {
+            btnSignUp.BackColor = Color.Black;
+            btnSignUp.ForeColor = Color.WhiteSmoke;
+        }
+    }
+}
